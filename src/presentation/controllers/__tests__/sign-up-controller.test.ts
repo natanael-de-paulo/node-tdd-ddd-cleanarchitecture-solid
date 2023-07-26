ac44b058d7c8294055437b13e3ae1ade4e250228
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-07-26 09:46:49 -0300

    refactor: changed name key of object http request

--- src/presentation/controllers/__tests__/sign-up-controller.test.ts
+++ src/presentation/controllers/__tests__/sign-up-controller.test.ts
@@ -8,7 +8,7 @@ describe('SignUp Controller', () => {
       body: {
         email: 'any_email@email.com',
         password: 'any_password',
-        password_confirmation: 'any_password'
+        passwordConfirmation: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
@@ -22,7 +22,7 @@ describe('SignUp Controller', () => {
       body: {
         name: 'any_name',
         password: 'any_password',
-        password_confirmation: 'any_password'
+        passwordConfirmation: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
@@ -36,7 +36,7 @@ describe('SignUp Controller', () => {
       body: {
         name: 'any_name',
         email: 'any_email@email.com',
-        password_confirmation: 'any_password'
+        passwordConfirmation: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
@@ -58,18 +58,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 
-  // it('Should return 200 if data is ok', () => {
-  //   const sut = new SignUpController()
-  //   const httpRequest = {
-  //     body: {
-  //       name: 'any_name',
-  //       email: 'any_email@email.com',
-  //       password: 'any_password',
-  //       password_confirmation: 'any_password'
-  //     }
-  //   }
-  //   const httpResponse = sut.handle(httpRequest)
-  //   expect(httpResponse.statusCode).toBe(200)
-  //   expect(httpResponse.body).toEqual(httpRequest.body)
-  // })
+  it('Should return 200 if data is ok', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual(httpRequest.body)
+  })
 })
