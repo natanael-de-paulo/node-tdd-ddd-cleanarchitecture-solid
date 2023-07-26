82493fb664e9f59d428423efe49ff7c48f750a17
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-07-26 09:39:28 -0300

    test: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/__tests__/sign-up-controller.test.ts
+++ src/presentation/controllers/__tests__/sign-up-controller.test.ts
@@ -44,23 +44,32 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  it('Should return 200 if data is ok', () => {
+  it('Should return 400 if no password confirmation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@email.com',
-        password: 'any_password',
-        password_confirmation: 'any_password'
+        password: 'any_password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body).toEqual({
-      name: 'any_name',
-      email: 'any_email@email.com',
-      password: 'any_password',
-      password_confirmation: 'any_password'
-    })
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
+
+  // it('Should return 200 if data is ok', () => {
+  //   const sut = new SignUpController()
+  //   const httpRequest = {
+  //     body: {
+  //       name: 'any_name',
+  //       email: 'any_email@email.com',
+  //       password: 'any_password',
+  //       password_confirmation: 'any_password'
+  //     }
+  //   }
+  //   const httpResponse = sut.handle(httpRequest)
+  //   expect(httpResponse.statusCode).toBe(200)
+  //   expect(httpResponse.body).toEqual(httpRequest.body)
+  // })
 })
