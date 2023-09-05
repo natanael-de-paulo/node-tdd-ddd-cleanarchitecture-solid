6b80895218abe6ac075b87284633349fc4bff362
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-09-05 09:27:11 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/__tests__/bcrypt-adapter.test.ts
+++ src/infra/criptography/__tests__/bcrypt-adapter.test.ts
@@ -25,4 +25,11 @@ describe('Bcrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
+
+  it('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }) as never)
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
